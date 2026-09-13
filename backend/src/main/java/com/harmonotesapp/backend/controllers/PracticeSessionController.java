bc03ebe7997d3e4f5ab7562cfe87410cafbfb239
B.Floyd <brooke.floyd10@gmail.com>
2026-09-13 01:02:57 -0500

    added columns, getters and setters for all fields needed to create practiceLog table, completed practiceSessionController mapping for full CRUD operations

--- backend/src/main/java/com/harmonotesapp/backend/controllers/PracticeSessionController.java
+++ backend/src/main/java/com/harmonotesapp/backend/controllers/PracticeSessionController.java
@@ -1,4 +1,41 @@
 package com.harmonotesapp.backend.controllers;
 
+import com.harmonotesapp.backend.models.PracticeSession;
+import com.harmonotesapp.backend.repositories.PracticeSessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class PracticeSessionController {
+    @Autowired
+    private PracticeSessionRepository practiceSessionRepository;
+
+@GetMapping("/api/practice-sessions")
+    public List<PracticeSession>getPracticeSessions() {
+    return practiceSessionRepository.findAll();
+}
+
+@GetMapping("/api/practice-sessions/{id}")
+    public PracticeSession getPracticeSessionsById(@PathVariable Long id) {
+    return practiceSessionRepository.findById(id).orElse(null);
+
+}
+
+@PostMapping("/api/practice-sessions/")
+    public PracticeSession createPracticeSession(@RequestBody PracticeSession practiceSession) {
+    return practiceSessionRepository.save(practiceSession);
+}
+
+@PutMapping("api/practice-sessions/{id}")
+    public PracticeSession updatePracticeSession(@PathVariable Long id, @RequestBody PracticeSession practiceSession) {
+    practiceSession.setId(id);
+    return practiceSessionRepository.save(practiceSession);
+}
+
+@DeleteMapping("/api/practice-sessions/{id}")
+    public void deletePracticeSession(@PathVariable Long id) {
+    practiceSessionRepository.deleteById(id);
+}
 }
--- backend/src/main/java/com/harmonotesapp/backend/models/PracticeSession.java
+++ backend/src/main/java/com/harmonotesapp/backend/models/PracticeSession.java
@@ -7,20 +7,16 @@ import jakarta.persistence.*;
         @Id
         @GeneratedValue(strategy=GenerationType.IDENTITY)
             private Long id;
-        @Column(name="session_topic")
-            private String sessionTopic;
+        @Column(name="session_focus")
+            private String sessionFocus;
+        @Column(name="session_duration")
+            private String sessionDuration;
+        @Column(name="session_triumphs_challenges")
+            private String sessionTriumphsChallenges;
 
         public PracticeSession() {
 
         }
-
-    public String getsessionTopic() {
-        return sessionTopic;
-    }
-
-    public void setSessionTopic(String sessionTopic) {
-        this.sessionTopic = sessionTopic;
-    }
 
     public Long getId() {
         return id;
@@ -28,5 +24,29 @@ import jakarta.persistence.*;
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getSessionFocus() {
+        return sessionFocus;
+    }
+
+    public void setSessionFocus(String sessionFocus) {
+        this.sessionFocus = sessionFocus;
+    }
+
+    public String getSessionDuration() {
+        return sessionDuration;
+    }
+
+    public void setSessionDuration(String sessionDuration) {
+        this.sessionDuration = sessionDuration;
+    }
+
+    public String getSessionTriumphsChallenges() {
+        return sessionTriumphsChallenges;
+    }
+
+    public void setSessionTriumphsChallenges(String sessionTriumphsChallenges) {
+        this.sessionTriumphsChallenges = sessionTriumphsChallenges;
     }
 }
