83185f2fb3cae1335c2bc7069f2cd85ec90bcbdc
B.Floyd <brooke.floyd10@gmail.com>
2026-09-13 02:04:54 -0500

    added @RequestMapping for controller class to reduce redundant code, added a services package and class for practiceSession with todo's for later to ensure they are completed

--- backend/src/main/java/com/harmonotesapp/backend/controllers/PracticeSessionController.java
+++ backend/src/main/java/com/harmonotesapp/backend/controllers/PracticeSessionController.java
@@ -8,33 +8,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/practice-sessions")
 public class PracticeSessionController {
     @Autowired
     private PracticeSessionRepository practiceSessionRepository;
 
-@GetMapping("/api/practice-sessions")
+@GetMapping()
     public List<PracticeSession>getPracticeSessions() {
     return practiceSessionRepository.findAll();
 }
 
-@GetMapping("/api/practice-sessions/{id}")
+@GetMapping("/{id}")
     public PracticeSession getPracticeSessionsById(@PathVariable Long id) {
     return practiceSessionRepository.findById(id).orElse(null);
 
 }
 
-@PostMapping("/api/practice-sessions/")
+@PostMapping()
     public PracticeSession createPracticeSession(@RequestBody PracticeSession practiceSession) {
     return practiceSessionRepository.save(practiceSession);
 }
 
-@PutMapping("api/practice-sessions/{id}")
+@PutMapping("/{id}")
     public PracticeSession updatePracticeSession(@PathVariable Long id, @RequestBody PracticeSession practiceSession) {
     practiceSession.setId(id);
     return practiceSessionRepository.save(practiceSession);
 }
 
-@DeleteMapping("/api/practice-sessions/{id}")
+@DeleteMapping("/{id}")
     public void deletePracticeSession(@PathVariable Long id) {
     practiceSessionRepository.deleteById(id);
 }
