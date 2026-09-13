de431bbfd7b46ef37d5f253ad19c5eb73803a3c6
B.Floyd <brooke.floyd10@gmail.com>
2026-09-13 02:57:01 -0500

    created User Repository and model complete with @Table annotation to avoid issued with the generic User entity name and getters and setters for all fields

---
+++ backend/src/main/java/com/harmonotesapp/backend/models/User.java
@@ -0,0 +1,85 @@
+package com.harmonotesapp.backend.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+        private Long id;
+    @Column(name="first_name")
+        private String firstName;
+    @Column(name="last_name")
+        private String lastName;
+    @Column(name="email_address")
+        private String emailAddress;
+    @Column(name="password")
+        private String password;
+    @Column(name="role")
+        private String role;
+    @Column(name="created_at")
+        private LocalDateTime createdAt;
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
