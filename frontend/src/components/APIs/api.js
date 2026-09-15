2a6be3bc5f645723288f2b325eb88165986ed20d
B.Floyd <brooke.floyd10@gmail.com>
2026-09-15 18:35:36 -0500

    Created reusable globalPost to use for posting ot database

--- frontend/src/components/APIs/api.js
+++ frontend/src/components/APIs/api.js
@@ -7,5 +7,45 @@ export async function globalGet(endPoint) {
     if(!response.ok) {
     throw new Error(`Error fetching data:${response.status}`);
     }
-    return response.json()
+    return response.json();
+}
+
+export async function globalPost(endPoint, postBody) {
+    const response = await fetch(`${apiBaseUrl}${endPoint}`, {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(postBody)
+    });
+
+    if(!response.ok) {
+    throw new Error(`Error saving data:${response.status}`);
+    }
+    return response.json();
+}
+
+export async function globalPut(endPoint, postBody) {
+    const response = await fetch(`${apiBaseUrl}${endPoint}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type" : "application/json"
+        }, 
+        body: JSON.stringify(postBody)
+    });
+
+    if(!response.ok) {
+    throw new Error(`Error updating data:${response.status}`);
+    }
+    return response.json();
+}
+
+export async function globalDelete(endPoint) {
+    const response = await fetch(`${apiBaseUrl}${endPoint}`, {
+        method: "DELETE"
+    });
+
+    if(!response.ok) {
+    throw new Error(`Error deleting data:${response.status}`);
+    }
 }
