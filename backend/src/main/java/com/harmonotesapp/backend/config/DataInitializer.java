08879a5cc0593afbe1c6fbe6a09a5f2ebf8009f1
B.Floyd <brooke.floyd10@gmail.com>
2026-09-18 01:31:44 -0500

    Changed from @PostConstruct to CommandLineRunner per SpringData and SpringBoot docs. added @Override on run method as a check to guarantee it matches commandLineRunners method exactly. Added user data i want to initialize the user with

--- backend/src/main/java/com/harmonotesapp/backend/config/DataInitializer.java
+++ backend/src/main/java/com/harmonotesapp/backend/config/DataInitializer.java
@@ -1,23 +1,33 @@
 package com.harmonotesapp.backend.config;
 
+import com.harmonotesapp.backend.models.User;
 import com.harmonotesapp.backend.repositories.StudentRepository;
 import com.harmonotesapp.backend.repositories.UserRepository;
-import jakarta.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 // hardcoded credentials until I can implement real auth
 
 @Component
-public class DataInitializer {
+public class DataInitializer implements CommandLineRunner {
     private final UserRepository userRepository;
     private final StudentRepository studentRepository;
 
     public DataInitializer(UserRepository userRepository, StudentRepository studentRepository) {
         this.userRepository = userRepository;
         this.studentRepository = studentRepository;
-}
+    }
 
-    @PostConstruct
-    public void initialize() {
+    @Override
+    public void run(String... args) throws Exception {
+        if (userRepository.count() == 0) {
+            User user = new User();
+            user.setFirstName("Gabriel");
+            user.setLastName("Floyd");
+            user.setEmailAddress("ChordRunner@gmail.com");
+            user.setPassword("K3ys&Ch0rds");
+            user.setRole("student");
+            userRepository.save(user);
+        }
     }
 }
