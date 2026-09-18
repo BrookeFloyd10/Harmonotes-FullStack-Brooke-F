9fc9d783f494f0d9ea56811fb29995263cb0b9f4
B.Floyd <brooke.floyd10@gmail.com>
2026-09-17 19:18:07 -0500

    imported login form, rendered it on the homepage and tested it in the ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import homepage from "../assets/images/homepage.svg";
+import "../components/LoginForm"
+import LoginSignupForm from "../components/LoginForm";
 
 const Home = () => {
     return(
-        <div className="home-page">
-            <img src={homepage} alt="artsy photo of guitar piano and drum sticks" />
-            <p>Where practice meets progress.</p>
-        </div>
+        <>
+            <div className="home-page">
+                <img src={homepage} alt="artsy photo of guitar piano and drum sticks" />
+                <p>Where practice meets progress.</p>
+            </div>
+
+            <LoginForm />
+        </>
     )
 }
 
