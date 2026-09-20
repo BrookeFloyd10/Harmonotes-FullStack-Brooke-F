db7fe51bebd55c1a84e95e8d6af5bb4785fef40f
B.Floyd <brooke.floyd10@gmail.com>
2026-09-20 17:33:06 -0500

    Created a reusable delete modal

---
+++ frontend/src/components/shared/DeleteModal.jsx
@@ -0,0 +1,25 @@
+import Button from "./Button"
+
+const DeleteModal = ({ handleDelete, onCancel, id, title }) => {
+    return (
+       <div className="delete-modal">
+
+        <h3>WAIT! Are you sure you want to delete {title}?</h3>
+             <Button 
+                className="delete-btn"
+                type="button"
+                onClick={() =>handleDelete(id)}
+                label= "Yes, Delete" />
+        
+            <Button 
+                className="cancel-btn"
+                type="button"
+                onClick={onCancel}
+                label="No, Cancel"
+        />
+        
+        </div> 
+    );
+};
+
+export default DeleteModal;
