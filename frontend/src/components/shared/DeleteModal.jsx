420e828776bf8bb6f3e77870b7969b4508a31f98
B.Floyd <brooke.floyd10@gmail.com>
2026-09-20 17:53:25 -0500

    Added an overlay wrapper to the delete modal for css styling, and styled it to appear to dim the page around it and stay centered on the screen

--- frontend/src/components/shared/DeleteModal.jsx
+++ frontend/src/components/shared/DeleteModal.jsx
@@ -2,22 +2,22 @@ import Button from "./Button"
 
 const DeleteModal = ({ handleDelete, onCancel, id, title }) => {
     return (
-       <div className="delete-modal">
-
-        <h3>WAIT! Are you sure you want to delete {title}?</h3>
-             <Button 
-                className="delete-btn"
-                type="button"
-                onClick={() =>handleDelete(id)}
-                label= "Yes, Delete" />
+      <div className="overlay-wrapper">
+        <div className="delete-modal">
+            <h3>WAIT! Are you sure you want to delete {title}?</h3>
+                <Button 
+                    className="delete-btn"
+                    type="button"
+                    onClick={() =>handleDelete(id)}
+                    label= "Yes, Delete" />
         
-            <Button 
-                className="cancel-btn"
-                type="button"
-                onClick={onCancel}
-                label="No, Cancel"
-        />
-        
+                <Button 
+                    className="cancel-btn"
+                    type="button"
+                    onClick={onCancel}
+                    label="No, Cancel"
+                />
+            </div> 
         </div> 
     );
 };
